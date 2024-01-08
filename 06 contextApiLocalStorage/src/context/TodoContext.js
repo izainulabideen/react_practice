2eace074cd92964dae1bf70da0b49f8003901af5
ZainRanaZr <zainrao417@gmail.com>
2024-01-08 16:38:09 +0500

    Context Api plus local Storage project

---
+++ 06 contextApiLocalStorage/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "Todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleCompleteTodo: (id) => {},
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+
+export const TodoContextProvider = TodoContext.Provider;
